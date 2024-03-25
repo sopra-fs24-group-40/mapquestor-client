7a5aeb2d1571216a2de5db8c159d42cb992c7d13
bhalf <benjamin.halfar@uzh.ch>
2024-03-25 16:17:37 +0100

    cleaned up directories

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React from "react";
-import Header from "./components/views/Header";
 import AppRouter from "./components/routing/routers/AppRouter";
 
-/**
- * Happy coding!
- * React Template by Lucas Pelloni
- * Overhauled by Kyrill Hux
- * Updated by Marco Leder
- */
 const App = () => {
   return (
     <div>
-      <Header height="100" />
       <AppRouter />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./styles/index.scss";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-/**
- * This is the entry point of your React application where the root element is in the public/index.html.
- * We call this a “root” DOM node because everything inside it will be managed by React DOM.
- * Applications built with just React usually have a single root DOM node.
- * More: https://react.dev/reference/react-dom/client/createRoot 
- */
+
 const container = document.getElementById("app");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(<App tab="home" />);
