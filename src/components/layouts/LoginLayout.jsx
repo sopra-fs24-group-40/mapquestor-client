90be600c8b40ad22a79af4c6cff8fa50b1dbedc0
LaughingF0x <115981865+LaughingF0x@users.noreply.github.com>
2024-04-03 12:25:21 +0200

    2 pics added and first draft of login page

--- src/components/layouts/LoginLayout.jsx
+++ src/components/layouts/LoginLayout.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
+import "../../styles/views/login.scss";
+import image from "../../assets/logo.png";
 
 function LoginLayout(props) {
-  return (
-    <div className="container">
-      <Outlet />
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-3 text-white p-5 text-start d-flex align-items-start">
+                    <img src={image} width="175"/>
+                </div>
+                <div className="col-md-3 text-black p-5 text-end d-flex align-items-end">
+                    <h1 className="fw-bold">MapQuestor</h1>
+                </div>
+            </div>
+            <Outlet/>
+        </div>
+    );
 }
 
 export default LoginLayout;
--- src/components/views/Login.tsx
+++ src/components/views/Login.tsx
@@ -2,24 +2,26 @@ import React from "react";
 import "../../styles/views/login.scss";
 
 function Login(props) {
-  return (
-    <div className="row">
-      <div className="col justify-content-center">
-        <h1 className="text-center">You need to login in order to see game</h1>
+    return (
+        <>
+            <div className="row justify-content-center">
+                <div className="col-md-5 justify-content-center rounded-4 p-4 login-container">
+                    <h1 className="text-center fw-bold">Login</h1>
 
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" className="form-control" id="username" placeholder="Enter username" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" id="password" placeholder="Password" />
-        </div>
-        <button type="submit" className="btn btn-primary mt-3">Login</button>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" id="username" placeholder="Enter username"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" id="password" placeholder="Password"/>
+                    </div>
+                    <button type="submit" className="btn btn-danger mt-3">Login</button>
 
-      </div>
-    </div>
-  );
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Login;
