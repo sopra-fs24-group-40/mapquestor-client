f56c9d7a870f65ab96b3d5b2b0feabf03edbb29c
LaughingF0x <115981865+LaughingF0x@users.noreply.github.com>
2024-04-04 15:43:06 +0200

    minor changes in GameLayout.jsx started to split game dashboard into 2 parts

--- src/components/layouts/GameLayout.jsx
+++ src/components/layouts/GameLayout.jsx
@@ -9,30 +9,30 @@ function GameLayout(props) {
     <div className="container-fluid">
       <div className="container-header row justify-content-center">
         <div className="col">
-          <div className="row align-items-center">
+          <div className="row align-items-center container-logoAndText">
             <div className="col-auto">
               <img src={logo} width={100}/>
             </div>
             <div className="container-mapquestor col-6">
-              <h1>MapQuestor</h1>
+            <h1>MapQuestor</h1>
             </div>
           </div>
         </div>
         <div className="col d-flex justify-content-end align-items-center">
-          <div className="container-search-bar col-auto p-3">
-            <p>Search other Users</p>
-            <label htmlFor="site-search"></label>
-            <input type="search" id="site-search" name="q"/>
-            <button>Search</button>
-          </div>
-          <div className="col-auto p-3">
-            <figure className="container-avatar">
-              <img src={avatar} width={50}/></figure>
-            <button>My Profile</button>
-          </div>
-          <div className="col-auto">
-            <button className="btn btn-danger">Logout</button>
-          </div>
+        <div className="container-search-bar col-auto p-3">
+              <p>Search other Users</p>
+              <label htmlFor="site-search"></label>
+              <input type="search" id="site-search" name="q"/>
+              <button>Search</button>
+            </div>
+            <div className="col-auto p-3">
+              <figure className="container-avatar">
+                <img src={avatar} width={50}/></figure>
+              <button>My Profile</button>
+            </div>
+            <div className="col-auto">
+              <button className="btn btn-danger">Logout</button>
+            </div>
         </div>
       </div>
       <div className="container">
--- src/components/views/Game/Game.tsx
+++ src/components/views/Game/Game.tsx
@@ -2,14 +2,19 @@ import React from "react";
 
 const Game = () => {
 
-  return (
-    <div className="row justify-content-center">
-      <div className="col-md-6 bg-white text-center mt-5">
-        <h1>Game Page</h1>
-        <button>Create Game</button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-6 bg-white text-center mt-5">
+                    <h1>Left Side</h1>
+                </div>
+                <div className="col-md-6 bg-white text-center mt-5">
+                    <h1>Right Side</h1>
+                    <button>Create Game</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Game;
