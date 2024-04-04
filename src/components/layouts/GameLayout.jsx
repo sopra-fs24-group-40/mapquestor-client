f6b26eaaca967f1a02b17fc56a67561554b79f03
B-M <branislav.milutinovic@uzh.ch>
2024-04-04 11:26:22 +0200

    dashboard first draft

--- src/components/layouts/GameLayout.jsx
+++ src/components/layouts/GameLayout.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import "../../styles/views/login.scss";
+import image from "../../assets/logo.png";
 
 function GameLayout(props) {
   return (
-    <div className="container">
+    <div className="container-fluid">
+      <div className="row justify-content-center">
+        <div className="col bg-white text-center">
+          <h1>MapQuestor</h1>
+          <img src={image} width="100"/>
+          <button>Create Game</button>
+        </div>
+    </div>
       <Outlet />
     </div>
   );
