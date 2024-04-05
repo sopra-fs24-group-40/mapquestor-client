e12537cf4b7775f6a40a0d1668f6d294fde6c456
LaughingF0x <115981865+LaughingF0x@users.noreply.github.com>
2024-04-05 10:23:56 +0200

    Dashboard looks wise finished

--- src/components/views/Game/Game.tsx
+++ src/components/views/Game/Game.tsx
@@ -3,48 +3,56 @@ import "../../../styles/views/game.scss";
 
 const Game = () => {
 
-  return (
-    <div className="row">
-      <div className="col-md-6 bg-white text-center mt-5">
-        <h1>Left Side</h1>
-        <div className="container-wrap">
-          <section id="leaderboard">
-            <nav className="ladder-nav">
-              <div className="ladder-title">
-                <h1>Leaderboard</h1>
-              </div>
-              <div className="ladder-search">
-                <input type="text" id="search-leaderboard" className="live-search-box"
-                  placeholder="Search Team, Player..." />
-              </div>
-            </nav>
-            <table id="rankings" className="leaderboard-results" width="100%">
-              <thead>
-                <tr>
-                  <th>Rank</th>
-                  <th>Name</th>
-                  <th>Total</th>
-                </tr>
-              </thead>
-            </table>
-          </section>
-        </div>
-      </div>
+    return (
+        <div className="row">
+            <div className="col-md-6 text-center mt-5">
+                <div className="container-wrap">
+                    <section id="leaderboard">
+                        <nav className="ladder-nav">
+                            <div className="ladder-title">
+                                <h1>Leaderboard</h1>
+                            </div>
+                            <div className="ladder-search">
+                                <select className="dropdown">
+                                    <option value="All Modes">All Modes</option>
+                                    <option value="City-Mode">City-Mode</option>
+                                    <option value="Country-Mode">Country-Mode</option>
+                                </select>
+                                <select className="dropdown">
+                                    <option value="All Time">All Time</option>
+                                    <option value="Today">Today</option>
+                                    <option value="This Week">This Week</option>
+                                    <option value="This Month">This Month</option>
+                                    <option value="This Year">This Year</option>
+                                </select>
+                            </div>
+                        </nav>
+                        <table id="rankings" className="leaderboard-results" width="100%">
+                            <thead>
+                            <tr>
+                                <th>Rank</th>
+                                <th>Name</th>
+                                <th>Total</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </section>
+                </div>
+            </div>
 
-      <div className="col-md-6 bg-white text-center mt-5">
-        <h1>Right Side</h1>
-        <div className="button-wrapper justify-content-center">
-          <button className="individual-button">All Users</button>
+            <div className="col-md-6 bg-transparent text-center mt-5">
+                <div className="button-wrapper justify-content-center">
+                    <button className="individual-button">All Users</button>
+                </div>
+                <div className="button-wrapper">
+                    <button className="individual-button">Join Lobby</button>
+                </div>
+                <div className="button-wrapper">
+                    <button className="individual-button">Create Game</button>
+                </div>
+            </div>
         </div>
-        <div className="button-wrapper">
-          <button className="individual-button">Join Lobby</button>
-        </div>
-        <div className="button-wrapper">
-          <button className="individual-button">Create Game</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Game;
