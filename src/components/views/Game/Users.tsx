23586624fc17275ac8ce99bf212010ca3aafcc21
B-M <branislav.milutinovic@uzh.ch>
2024-04-05 10:28:03 +0200

    added funtion to fetch users

--- src/components/views/Game/Users.tsx
+++ src/components/views/Game/Users.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { api } from "helpers/api";
 
 function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await api.get("/users");
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        setUsers(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchUsers();
+  }, []);
+
+  if (users) {
+    
+  }
+
   return (
     <div className="row">
       <div className="col bg-light mt-3 border rounded">
         <h1 className="text-center p-2">All Users</h1>
-        <button>Username (Me)</button>
       </div>
     </div>
   );
