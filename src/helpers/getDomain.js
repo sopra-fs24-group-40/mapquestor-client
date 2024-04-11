8f688553d6ce2b6ff897b83d9145c46cbdcd6482
bhalf <benjamin.halfar@uzh.ch>
2024-04-11 12:37:46 +0200

    Changed ws host / cleaned up files

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -7,7 +7,7 @@ import { isProduction } from "./isProduction";
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = "https://sopra-fs24-group-40-server1.oa.r.appspot.com/"
+  const prodUrl = "https://sopra-fs24-group-40-server1.oa.r.appspot.com"
   const devUrl = "http://localhost:8080";
 
   return isProduction() ? prodUrl : devUrl;
