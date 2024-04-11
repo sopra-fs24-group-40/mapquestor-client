0ec6be1613bee310ac3af9739d0136b379d1287e
Arber Markaj <arber.markaj@uzh.ch>
2024-04-11 14:11:46 +0200

    Return map #29

--- src/components/views/game/gameparts/Ingame.jsx
+++ src/components/views/game/gameparts/Ingame.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Loader } from '@googlemaps/js-api-loader';
 
-function Ingame(props) {
+const InGame = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const apiOptions = {
+      apiKey: "AIzaSyDKZd3AoAgVQyvXXGptbGAnpmBBzLbXG0A" // Replace with your Google Maps API key
+    };
+
+    const loader = new Loader(apiOptions);
+
+    loader.load().then(() => {
+      const streetView = new google.maps.StreetViewPanorama(
+        document.getElementById("street-view"), {
+          position: { lat: 37.86926, lng: -122.254811 },
+          pov: { heading: 165, pitch: 0 },
+          zoom: 1,
+        }
+      );
+    }).catch(error => {
+      console.error('Error loading Google Maps API:', error);
+    });
+  }, []);
+
   return (
-    <div className="row justify-content-center mt-5">
-      <div className={"col-md-6 bg-white text-black"}>
-        <h1>Ingame container stylen</h1>
-      </div>
+    <div>
+      <h1>In Game</h1>
+      <div id="street-view" style={{ width: '100%', height: '400px' }}></div>
     </div>
   );
 }
 
-export default Ingame;
+export default InGame;
