b71fc2b8bff6ca33c5f24ddb882a58d057f3cc90
Nikola Stevanovic <nikola.stevanovic@uzh.ch>
2024-04-13 14:16:54 +0200

    version 1.0 User-friendly game screen #17

--- src/components/views/game/gameparts/Ingame.jsx
+++ src/components/views/game/gameparts/Ingame.jsx
@@ -15,7 +15,7 @@ const InGame = () => {
     loader.load().then(() => {
       const streetView = new google.maps.StreetViewPanorama(
         document.getElementById("street-view"), {
-          position: { lat: 37.86926, lng: -122.254811 },
+          position: { lat: 47.3786, lng: 8.5400 },
           pov: { heading: 165, pitch: 0 },
           zoom: 1,
         }
@@ -26,13 +26,13 @@ const InGame = () => {
   }, []);
 
   return (
-    <div className="row">
-      <div className="col-md-6 text-center mt-5">
+    <div className="row mt-5">
+      <div className="col-md-3 text-center">
         <div className="container-wrap">
           <section id="leaderboard">
-            <nav className="ladder-nav" >
+            <nav className="ladder-nav">
               <div className="ladder-title">
-                <h1>Leaderboard</h1>
+              <h1 style={{ fontSize: '20px' }}>Leaderboard</h1>
               </div>
             </nav>
             <table id="rankings" className="leaderboard-results" width="100%">
@@ -46,14 +46,27 @@ const InGame = () => {
           </section>
         </div>
       </div>
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <div className="col-md-6 justify-content-center text-center">
       <h1>In Game</h1>
-      <div id="street-view" style={{ width: "50%", height: "350px" }}></div>
+      <div id="street-view" style={{ width: "100%", height: "350px" }}></div>
+      
       <div className="button-wrapper">
-          <button className="individual-button" >Delay Joker</button>
-          <button className="individual-button" >Hint remove Joker</button>
+          <button className="individual-button" style={{ fontSize: '20px' }} >Delay Joker</button>
+          <button className="individual-button" style={{ fontSize: '20px' }} >Hint remove Joker</button>
         </div>
     </div>
+  
+      <div className="col-md-3 text-center">
+        <div className="container-wrap">
+          <section id="leaderboard">
+            <nav className="ladder-nav">
+              <div className="ladder-title">
+              <h1 style={{ fontSize: '28px' }}>Chat</h1>
+              </div>
+            </nav>
+          </section>
+        </div>
+      </div>
     </div>
   );
 };
